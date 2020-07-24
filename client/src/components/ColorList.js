0020f1e2dd29cc60e32fc677950a5ddd08eed130
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-24 14:05:29 -0700

    met mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -27,7 +27,7 @@ const ColorList = ({ colors, updateColors, setColorList}) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    axios
+    axiosWithAuth()
     .put(`http://localhost:5000/api/colors/${id}`, colorToEdit)
     .then(res => {
       updateColors(res.data);
