a00ef1ffd9e7730ce989f8994c9a11aad2a7f88e
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-24 09:15:44 -0700

    got login form and colors rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./utils/PrivateRoute";
+import BubblePage from "./components/BubblePage";
+import ColorList from "./components/ColorList";
 
 function App() {
+  const [colorList, setColorList] = useState([])
+
+  const updateColor = updatedColor => {
+    const updatedColors = colorList.map(colors => {
+      if(colors.id === updatedColor.id){
+        return updatedColor;
+      }
+      return colors;
+    });
+    setColorList(updatedColors);
+  }
   return (
     <Router>
       <div className="App">
+        <nav>
+          <Link to ="/">Login</Link>
+          <Link to ="/bubblepage">View Colors</Link>
+          <Link to ="/colorlist">Update Colors</Link>
+        </nav>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/bubblepage" component={BubblePage}/>
+        <PrivateRoute exact path="/colorlist" component={ColorList} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const initialLoginFormValues = {
+  username: '',
+  password: ''    
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [loginFormValues, setLoginFormValues] = useState(initialLoginFormValues);
+
+  const onInputChange = e => {
+    setLoginFormValues({
+        ...loginFormValues,
+        [e.target.name]: e.target.value
+     })
+}
+
+  const onLogin = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('/api/login', loginFormValues)
+    .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubblepage");
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+        <form onSubmit={onLogin}>
+          <label> Username:
+            <input 
+              id='username'
+              name='username'
+              type='text'
+              value={loginFormValues.username}
+              onChange={onInputChange}
+            />
+          </label>
+          <label>Password:
+            <input 
+              id='password'
+              name='password'
+              type='text'
+              value={loginFormValues.password}
+              onChange={onInputChange}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
     </>
   );
 };
